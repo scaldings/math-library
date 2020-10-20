a1b481157405071c85cd3774fc3e07bb1c9ebd72
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-20 17:06:54 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='math-lib',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/scaldings/math-library.git',
+    license='',
+    author='scaldings',
+    author_email='',
+    description=''
+)
